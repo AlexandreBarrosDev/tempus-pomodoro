7fcc8e5f22a3f1d7cff8d2d7edf4299601478b3c
Alexandre Barros <alexandrebarros.dev@gmail.com>
2025-04-13 12:06:11 -0300

    adiciona context provider para controlar o estado dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,26 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TaskContextProvider } from './contexts/TaskContext';
+import { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formatSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 1,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export default function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
